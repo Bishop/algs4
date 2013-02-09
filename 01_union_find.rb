a70728a0d1cda78a719b3015504a39d07ead83b7
Alexandr Prudnikov <al.prudnikov@gmail.com>
2013-02-09 21:04:21 +0300

    Refactoring; added QuickUnion skeleton

--- 01_union_find.rb
+++ 01_union_find.rb
@@ -1,8 +1,14 @@
-class QuickFind
+class UF
     def initialize(capacity)
         @id = (0...capacity).to_a
     end
 
+    def to_s
+        @id.map(&:to_s).join(' ')
+    end
+end
+
+class QuickFind < UF
     def connected?(p, q)
         @id[p] == @id[q]
     end
@@ -12,18 +18,39 @@ class QuickFind
         idq = @id[q]
         @id.map! { |id| id == idp ? idq : id }
     end
+end
 
-    def to_s
-        @id.map(&:to_s).join(' ')
+class QuickUnion < UF
+    def initialize(capacity)
+        super
+        @sz = [1] * capacity
+    end
+
+    def connected?(p, q)
+    end
+
+    def union(p, q)
+    end
+
+    private
+
+    def root(i)
     end
 end
 
-qf = QuickFind.new 10
-puts qf.to_s
+size = 10
+algs = [QuickFind, QuickUnion]
 
-qf.union(1, 3);
-qf.union(5, 9);
-qf.union(8, 3);
+for alg in algs
+    puts alg.name
 
-puts qf.to_s
-puts qf.connected?(1, 8).inspect
+    qf = alg.new size
+    puts qf.to_s
+
+    qf.union(1, 3);
+    qf.union(5, 9);
+    qf.union(8, 3);
+
+    puts qf.to_s
+    puts qf.connected?(1, 8).inspect
+end
