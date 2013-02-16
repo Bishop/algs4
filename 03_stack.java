d70eb0342bdcc867b4df453decf7cc4d191c9584
Alexandr Prudnikov <al.prudnikov@gmail.com>
2013-02-17 00:47:25 +0300

    Fix array stack

--- 03_stack.java
+++ 03_stack.java
@@ -56,14 +56,22 @@ class ArrayStackOfStrings
 
     public String pop()
     {
-        return s[--N];
+        String item = s[--N];
+        s[N] = null;
+
+        if (N > 0 && N == s.length / 4)
+        {
+            resize(s.length / 2);
+        }
+
+        return item;
     }
 
     private void resize(int capacity)
     {
         String[] copy = new String[capacity];
 
-        System.arraycopy(s, 0, copy, 0, s.length);
+        System.arraycopy(s, 0, copy, 0, N);
 
         s = copy;
     }
