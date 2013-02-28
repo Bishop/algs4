a69848a9dbe76c7bb35312bf8da8cd3d451611dc
Alexandr Prudnikov <al.prudnikov@gmail.com>
2013-03-01 01:05:49 +0300

    Sorting

---
+++ 04_sort.java
@@ -0,0 +1,57 @@
+abstract class SortAlg
+{
+    protected static boolean less(Comparable v, Comparable w)
+    {
+        return v.compareTo(w) < 0;
+    }
+
+    protected static void exch(Comparable[] a, int i, int j)
+    {
+        Comparable tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
+
+class Selection extends SortAlg
+{
+    public static void sort(Comparable[] a)
+    {
+        int N = a.length;
+        for (int i = 0; i < N; i++)
+        {
+            int min = i;
+            for (int j = i + 1; j < N; j++)
+                if (less(a[j], a[min]))
+                    min = j;
+            exch(a, i, min);
+        }
+    }
+}
+
+class Insertion extends SortAlg
+{
+    public static void sort(Comparable[] a)
+    {
+        int N = a.length;
+        for (int i = 0; i < N; i++)
+            for (int j = i; j > 0; j--)
+                if (less(a[j], a[j - 1]))
+                    exch(a, j, j - 1);
+                else break;
+    }
+}
+
+class Experiment
+{
+    public static void main(String[] args)
+    {
+        int N = Integer.parseInt(args[0]);
+        Double[] a = new Double[N];
+        for (int i = 0; i < N; i++)
+            a[i] = StdRandom.uniform();
+        Insertion.sort(a);
+        for (int i =0; i < N; i++)
+            StdOut.println(a[i]);
+    }
+}
