fc6406336672fa2025bb18ff72f4203235085581
Alexandr Prudnikov <al.prudnikov@gmail.com>
2013-03-01 01:26:37 +0300

    Shellsort

--- 04_sort.java
+++ 04_sort.java
@@ -42,6 +42,27 @@ class Insertion extends SortAlg
     }
 }
 
+class Shell extends SortAlg
+{
+    public static void sort(Comparable[] a)
+    {
+        int N = a.length;
+
+        int h = 1;
+        while (h < N/3) h = 3*h + 1;
+
+        while (h >= 1)
+        {
+            //h-sort the array
+            for (int i = h; i < N; i++)
+            {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h)
+                    exch(a, j, j - h);
+            }
+        }
+    }
+}
+
 class Experiment
 {
     public static void main(String[] args)
